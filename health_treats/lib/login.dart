7c9f1244073d120564dc4eda97d90657b0a75510
MilitaoMatheus <matheus.silva2533@etec.sp.gov.br>
2024-09-13 21:29:54 -0300

    Adicionando a validação de dados á tela de Login

--- health_treats/lib/login.dart
+++ health_treats/lib/login.dart
@@ -5,30 +5,17 @@ import 'package:flutter/services.dart';
 import 'package:health_treats/cadastro.dart';
 import 'package:health_treats/menu.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
   
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: HomeScreen());
-  }
+  _LoginState createState() => _LoginState();
 }
 
-
-class HomeScreen extends StatefulWidget {
-  
-  const HomeScreen({super.key});
-  
-  @override
-  // ignore: library_private_types_in_public_api
-  _MainAppState createState() => _MainAppState();
-}
-
-// Classe ue contém todo o conteúdo a ser exibido na tela
-class _MainAppState extends State<HomeScreen> {
+// Classe que contém todo o conteúdo a ser exibido na tela
+class _LoginState extends State<Login> {
   bool _obscureText = true; // Controla a visibilidade da senha
-
+  final _formKey = GlobalKey<FormState>();  // Chave para o formulário
 
   @override
   Widget build(BuildContext context) {
@@ -93,41 +80,41 @@ class _MainAppState extends State<HomeScreen> {
                 ), // Fim do Text
               ), // Fim do Center
 
-                Padding( // Comando utilizado para definir as distâncias de um elemento para a margem 
+              Padding( // Comando utilizado para definir as distâncias de um elemento para a margem 
                 padding: const EdgeInsets.all(30.0), // Definido a distância das margens laterais
-                child: Column( // Criando uma coluna dentro do Padding
-                  crossAxisAlignment: CrossAxisAlignment.start, // Alinha o texto à esquerda
-                  children: <Widget>[ // Indica que widgets serão criados
-                    const Text( // Criando o texto que irá acima de um TextFormField
-                      'E-mail', // Texto a ser exibido
-                      style: TextStyle( // Comando utilizado para estilizar o texto
-                        fontSize: 18.0, // Definindo o tamanho da fonte
-                        fontWeight: FontWeight.bold, // Definindo o estilo da fonte (itálico, negrito, etc)
+                child: Form(
+                  key: _formKey, // Associando o formulário ao _formKey
+                  child: Column( // Criando uma coluna dentro do Padding
+                    crossAxisAlignment: CrossAxisAlignment.start, // Alinha o texto à esquerda
+                    children: <Widget>[ // Indica que widgets serão criados
+                      const Text( // Criando o texto que irá acima de um TextFormField
+                        'E-mail', // Texto a ser exibido
+                        style: TextStyle( // Comando utilizado para estilizar o texto
+                          fontSize: 18.0, // Definindo o tamanho da fonte
+                          fontWeight: FontWeight.bold, // Definindo o estilo da fonte (itálico, negrito, etc)
 
-                        // O comando abaixo foi utlizado para retirar um sublinhado de aviso
-                        // ignore: use_full_hex_values_for_flutter_colors
-                        color: Color(0xff000000), // Definindo a cor do texto
-                      ) // Fim do TextStyle
-                    ), // Fim do Text
-
-                    const SizedBox(height: 10.0), // Comando utilizado para dar quebras de pixels na tela 
-
-                    TextFormField( // Comando utilizado para a criação de um campo de um formulário de Login
-                      keyboardType: TextInputType.emailAddress, // Comando que define o tipo de entrada de dado no teclado (Email)
-                      inputFormatters: <TextInputFormatter>[ // Formatando o que será recebido do teclado
-                        FilteringTextInputFormatter.singleLineFormatter, 
-                      ],
-                                            
-                      decoration: InputDecoration( //Decoração da caixa de entrada de dados
-                        labelText: 'Digite aqui', // Hint a ser exibido dentro da caixa de entrada de dados
-                        labelStyle: const TextStyle(
+                          // O comando abaixo foi utlizado para retirar um sublinhado de aviso
                           // ignore: use_full_hex_values_for_flutter_colors
-                          color: Color(0xff93B6EE),
-                        ),
-                         enabledBorder: const OutlineInputBorder( // Definido o estilo da borda da caixa de entrada de dados quando não focado
+                          color: Color(0xff000000), // Definindo a cor do texto
+                        ) // Fim do TextStyle
+                      ), // Fim do Text
+
+                      const SizedBox(height: 10.0), // Comando utilizado para dar quebras de pixels na tela 
+
+                      TextFormField( // Comando utilizado para a criação de um campo de um formulário de Login
+                        keyboardType: TextInputType.emailAddress, // Comando que define o tipo de entrada de dado no teclado (Email)
+                        inputFormatters: <TextInputFormatter>[ // Formatando o que será recebido do teclado
+                          FilteringTextInputFormatter.deny(RegExp('[\\n]')), // Permitindo apenas uma linha de texto
+                        ],
+                        decoration: InputDecoration( //Decoração da caixa de entrada de dados
+                          labelText: 'Digite aqui', // Hint a ser exibido dentro da caixa de entrada de dados
+                          labelStyle: const TextStyle(
+                            // ignore: use_full_hex_values_for_flutter_colors
+                            color: Color(0xff93B6EE),
+                          ),
+                          enabledBorder: const OutlineInputBorder( // Definido o estilo da borda da caixa de entrada de dados quando não focado
                             borderSide: BorderSide(color: Color(0xFF000000)), // Cor da borda quando não está focado
                           ),
-
                           focusedBorder: const OutlineInputBorder( // Definido o estilo da borda da caixa de entrada de dado quando focada
 
                             // O comando abaixo foi utlizado para retirar um sublinhado de aviso
@@ -138,81 +125,115 @@ class _MainAppState extends State<HomeScreen> {
                           border: OutlineInputBorder( // Estilizando a borda da caixa de entrada de dados em geral
                             borderRadius: BorderRadius.circular(30.0), // Arredondamento das bordas
                           ), // Fim da decoração da borda
-                      ), // Fim da decoração do input
-                    ), // Fim do forms
-                    
-                    const SizedBox(height: 20.0), // Comando utilizado para dar quebras de pixels na tela
+                          errorBorder:  const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              // Alterando a cor do erro para azul
+                              color: Color(0xff93B6EE),  
+                            ),
+                          ),
+                          // Estilizando a caixa de entrada quando dá erro -> focado
+                          focusedErrorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              // Alterando a cor do erro para azul
+                              color: Color(0xff93B6EE),
+                            ),
+                          ),
+                        ), // Fim da decoração do input
+                        validator: (value) {
+                          RegExp emailRegex = RegExp(
+                            r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor insira um email válido';
+                          } else if (!emailRegex.hasMatch(value)) {
+                            return 'Email inválido';
+                          }
+                          return null;
+                        },
+                      ), // Fim do forms
+                      
+                      const SizedBox(height: 20.0), // Comando utilizado para dar quebras de pixels na tela
 
-                    const Text( // Criando o texto que irá acima de um TextFormField
-                      'Senha', // Texto a ser exibido
-                      style: TextStyle( // Comando utilizado para estilizar o texto
-                        fontSize: 18.0, // Definindo o tamanho da fonte
-                        fontWeight: FontWeight.bold, // Definindo o estilo da fonte (itálico, negrito, etc)
+                      const Text( // Criando o texto que irá acima de um TextFormField
+                        'Senha', // Texto a ser exibido
+                        style: TextStyle( // Comando utilizado para estilizar o texto
+                          fontSize: 18.0, // Definindo o tamanho da fonte
+                          fontWeight: FontWeight.bold, // Definindo o estilo da fonte (itálico, negrito, etc)
 
-                        // O comando abaixo foi utlizado para retirar um sublinhado de aviso
-                        // ignore: use_full_hex_values_for_flutter_colors
-                        color: Color(0xff000000), // Definindo a cor do texto
-                      ) // Fim do TextStyle
-                    ), // Fim do Text
-
-                    const SizedBox(height: 10.0), // Comando utilizado para dar quebras de pixels na tela
-
-                    TextFormField( // Comando utilizado para a criação de um campo de um formulário de Login
-                      keyboardType: TextInputType.text, // Comando que define o tipo de entrada de dado no teclado (tipo texto)
-                      obscureText: _obscureText, // Váriavel que fornece a opção de visibilidade de senha
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.singleLineFormatter // Formatando o que será recebido do teclado
-                      ],
-
-                      decoration: InputDecoration( //Decoração da caixa de entrada de dados
-                        labelText: 'Digite aqui', // Hint a ser exibido dentro da caixa de entrada de dados
-                        labelStyle: const TextStyle(
+                          // O comando abaixo foi utlizado para retirar um sublinhado de aviso
                           // ignore: use_full_hex_values_for_flutter_colors
-                          color: Color(0xff93B6EE),
-                        ),
-                         enabledBorder: const OutlineInputBorder( // Definido o estilo da borda da caixa de entrada de dados quando não focado
+                          color: Color(0xff000000), // Definindo a cor do texto
+                        ) // Fim do TextStyle
+                      ), // Fim do Text
+
+                      const SizedBox(height: 10.0), // Comando utilizado para dar quebras de pixels na tela 
+
+                      TextFormField( //Comando utilizado para a criação de um campo de um formulário de senha
+                        obscureText: _obscureText, // Ocultar ou exibir o texto da senha
+                        decoration: InputDecoration( // Decoração da caixa de entrada de dados
+                          labelText: 'Digite aqui', // Texto a ser exibido
+                          labelStyle: const TextStyle(
+                            // ignore: use_full_hex_values_for_flutter_colors
+                            color: Color(0xff93B6EE), // Cor do texto que ficará na label
+                          ),
+                          enabledBorder: const OutlineInputBorder( // Definido o estilo da borda da caixa de entrada de dados quando não focado
                             borderSide: BorderSide(color: Color(0xFF000000)), // Cor da borda quando não está focado
                           ),
-
                           focusedBorder: const OutlineInputBorder( // Definido o estilo da borda da caixa de entrada de dado quando focada
-
-                            // O comando abaixo foi utlizado para retirar um sublinhado de aviso
                             // ignore: use_full_hex_values_for_flutter_colors
                             borderSide: BorderSide(color:  Color(0xff93B6EE)), // Cor da borda quando está focado
                           ),
-
                           border: OutlineInputBorder( // Estilizando a borda da caixa de entrada de dados em geral
                             borderRadius: BorderRadius.circular(30.0), // Arredondamento das bordas
                           ),
-
-                          suffixIcon: IconButton( // Icon de visualização de senha
-
-                            // O comando abaixo foi utlizado para retirar um sublinhado de aviso
-                            // ignore: use_full_hex_values_for_flutter_colors
-                            color: const Color(0xff93B6EE), // Cor do Icon
-                            icon: Icon(
-                              _obscureText ? Icons.visibility : Icons.visibility_off, // Função que deixa a senha visível/invisível
+                           errorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xff93B6EE),  // Alterar a cor do erro para azul
                             ),
-                            onPressed: (){
-                              setState((){
-                                _obscureText = !_obscureText;
+                          ),
+                          // Borda de erro quando o campo está focado
+                          focusedErrorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xff93B6EE),  // Outra cor para quando o campo está focado
+                            ),
+                          ),
+                          suffixIcon: IconButton( // Ícone para ocultar ou mostrar a senha
+                            icon: Icon(
+                              _obscureText ? Icons.visibility : Icons.visibility_off, // Mudar ícone de acordo com o estado
+                              color: const Color(0xFF93B6EE),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText; // Alterar visibilidade ao clicar no ícone
                               });
-                            }
-                          ) // Fim do comando de estilização do Icon (junto com a função)
-                      ), // Fim do TextStyle
-                    ), // Fim do TextFormField
+                            },
+                          ),
+                        ),
+                        validator: (value) { // Validação do campo de senha
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor insira uma senha';
+                          } else if (value.length < 6) {
+                            return 'A senha deve ter pelo menos 6 caracteres';
+                          }
+                          return null;
+                        },
+                      ), // Fim do forms
 
                     const SizedBox(height: 20.0), // Comando utilizado para dar quebras de pixels na tela
 
                     SizedBox( // Definição de um espaço que servirá como botão
                       width: double.infinity, // Ocupa toda a largura disponível
                       child: ElevatedButton( // Criando o botão de entrar
-                        onPressed: () { // Método de navegação ao clicar no botão
-                           Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const MenuApp())
-                          );
-                        },
+                        onPressed: () {
+                            // Verifica se o formulário é válido
+                            if (_formKey.currentState?.validate() ?? false) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const MenuApp(),
+                                ),
+                              );
+                            }
+                          },
                         style: ElevatedButton.styleFrom( // Estilizando o botão
 
                           // O comando abaixo foi utlizado para retirar um sublinhado de aviso
@@ -261,7 +282,7 @@ class _MainAppState extends State<HomeScreen> {
                     ),
                   ), // Fim do Align
 
-                  GestureDetector( // Captação de gesto para o icon
+                    GestureDetector( // Captação de gesto para o icon
                       onTap: () { // Metodo chamado ao clicar no icon
                         Navigator.push(
                             context,
@@ -287,6 +308,7 @@ class _MainAppState extends State<HomeScreen> {
                   ],
                 )
               ),
+            ),
             ],
           ),
         ),
