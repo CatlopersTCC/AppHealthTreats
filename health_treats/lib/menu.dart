c226a7330436edded3e3c9c78bbcd03fe6bcba47
MKevin2 <matheus.santos1616@etec.sp.gov.br>
2024-09-15 00:45:24 -0300

    Criando o bottomNavigationBar da tela inicial

--- health_treats/lib/menu.dart
+++ health_treats/lib/menu.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:health_treats/login.dart';
 
 class MenuApp extends StatelessWidget {
-  const MenuApp ({super.key});
+  const MenuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,50 +16,56 @@ class MenuApp extends StatelessWidget {
   }
 }
 
-class MenuInicio extends StatefulWidget{
+class MenuInicio extends StatefulWidget {
   const MenuInicio({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MenuInicio createState() => _MenuInicio();
 }
 
-class _MenuInicio extends State<MenuInicio>{
+class _MenuInicio extends State<MenuInicio> {
   TextEditingController _searchController = TextEditingController();
-  
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Login()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
+      body: SingleChildScrollView (
+         padding: const EdgeInsets.all(20.0),
+          child: Column(
+           children: [
             TextField(
               controller: _searchController,
               decoration: InputDecoration(
-                hintText: 'Pesuisar',
+                hintText: 'Pesquisar',
                 prefixIcon: const Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(50.0),
                 ),
               ),
               onChanged: (value) {
-                // Lógica de pesquisa pode ser adicionada aqui
+
+                //Lógica de pesquisa pode ser adicionada aqui
                 print('Texto digitado: $value');
               },
             ),
-            
-            //COMEÇA OS NOVOS CÓDIGOS A PARTIR DAQUI
-
             const SizedBox(height: 16.0),
-            
-            //TESTE DE CÓDIGO (SUBSTITUIR DEPOIS)
             const Padding(
               padding: EdgeInsets.all(30.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const Text(
+                  Text(
                     'Nome de usuário',
                     style: TextStyle(
                       fontSize: 18.0,
@@ -66,15 +73,48 @@ class _MenuInicio extends State<MenuInicio>{
                       color: Color(0xff000000),
                     ),
                   ),
-
-
-                ]
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0), //Coloca padding simétrico, tanto na esquerda, como na direita
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30.0), // Arredonda apenas o canto superior esquerdo
+          topRight: Radius.circular(30.0), // Arredonda apenas o canto superior direito
+        ), 
+          child: BottomNavigationBar (
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.messenger_outline_rounded),
+                //Label não pode ser nula, se não dá erro
+                label: '',
+              ),
+          
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: '',
+              ),
+          
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_circle_outline), 
+                label: '',
+              ),
+            ],
+          currentIndex: _selectedIndex, //Posição
+          selectedItemColor: Colors.blue, //Botão selecionado
+          unselectedItemColor: Colors.blue, //Botão deselecionado
+          backgroundColor: const Color.fromARGB(255, 252, 241, 241),
+          onTap: _onItemTapped, //Função acionada ao clicar
+          showSelectedLabels: false, //Evita que a label apareça quando selecionado
+          showUnselectedLabels: false, //Evita que a label apareça quando deselecionado
+          ),
+          ),
+         )
     );
-    
   }
 }
+
